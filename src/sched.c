7b913ec4d3ecf62cd580674fa076cabe3f392519
GuillaumeFerron <gferron@TC405-114-06.insa-lyon.fr>
2017-11-20 11:57:39 +0100

    following of sched exercise

--- src/sched.c
+++ src/sched.c
@@ -1,22 +1,38 @@
 #include "./sched.h"
+#include "./syscall.h"
 
 //System call modes
 int SYSCALL_SCHED_NUMBER = 5;
 
 //Executed process
 struct pcb_s *current_process;
+struct pcb_s kmain_process;
+
+
+/*************** INITIALIZATION ***************/
+
+void sched_init() {
+	current_process = &kmain_process;
+}
+
 
 /*************** SCHED ***************/
 
 void sys_yieldto(struct pcb_s* dest) {
 	
-	__asm("mov r0, %0" : : "r"(SYSCALL_SCHED_NUMBER) : "r0");
-	__asm("mov r1, %0" : : "r"(dest) : "r1", "r0");
-	
+	__asm("mov r0, %0" : : "r"(SYSCALL_SCHED_NUMBER) : "r0", "r1");
+	__asm("mov r1, %0" : : "r"(dest) : "r0", "r1");
+
 	//Interruption Call
 	__asm("SWI 0");
 }
 
 void do_sys_yieldto() {
+	
+	for(int i = 0; i < sizeof((current_process->regs)); i++) {
+		(current_process->regs)[i] = *(regs + i);	
+	}
+
+	current_process = (struct pcb_s*) *(regs + 1);
 	
 }
--- src/sched.h
+++ src/sched.h
@@ -6,10 +6,12 @@
 
 /*************** type declaration *****************/
 struct pcb_s {
-	uint32_t* regs;
+	uint32_t regs[14];
 };
 
 /*************** Functions declaration *****************/
+void sched_init();
+
 void sys_yieldto(struct pcb_s* dest);
 void do_sys_yieldto();
 
