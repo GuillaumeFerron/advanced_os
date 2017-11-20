c90876d8aba5484ace1956f0d7d0872f1e4f9296
GuillaumeFerron <gferron@TC405-114-06.insa-lyon.fr>
2017-11-20 11:55:47 +0100

    main refactored to launch two processes

--- kmain.c
+++ kmain.c
@@ -1,36 +1,46 @@
 #include "./src/syscall.h"
 #include <stdint.h>
 #include <inttypes.h>
+#include "./src/util.h"
+#include "sched.h"
 
-void
-dummy()
-{
-  return;
-}
-int
-div(int dividend, int divisor)
-{
-  int result = 0;
-  int remainder = dividend;
-  while (remainder >= divisor) {
-    result++;
-    remainder -= divisor;
-  }
-  return result;
-}
-int
-compute_volume(int rad)
-{
-  int rad3 = rad * rad * rad;
-  return div(4*355*rad3, 3*113);
-}
-void
-kmain( void )
-{
-  __asm("cps 0x10"); // switch CPU to USER mode
-    // **********************************************************************
+//Function declarations
+void user_process_1();
+void user_process_2();
 
-    uint64_t time = sys_gettime();
+//PCBs
+struct pcb_s pcb1, pcb2;
+struct pcb_s *p1, *p2;
 
-    time++; // suppress compiler error
+void kmain( void ) {
+	sched_init();
+	p1=&pcb1;
+	p2=&pcb2;
+
+	// initialize p1 and p2
+	p1->regs[13] = (uint32_t) &user_process_1;
+	p2->regs[13] = (uint32_t) &user_process_2;
+
+	__asm("cps 0x10"); // switch CPU to USER mode
+	// **********************************************************************
+	sys_yieldto(p1);
+
+	// this is now unreachable
+	PANIC();
+}
+
+void user_process_1() {
+	int v1=5;
+	while(1) {
+		v1++;
+		sys_yieldto(p2);
+	}
+}
+
+void user_process_2() {
+	int v2=-12;
+	while(1) {
+		v2-=2;
+		sys_yieldto(p1);
+	}
 }
