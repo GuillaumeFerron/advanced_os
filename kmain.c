36e6803af8a045a1ae87e9f7ec56937258226b8b
GuillaumeFerron <gferron@TC405-112-05.insa-lyon.fr>
2017-12-06 08:56:19 +0100

    code reindentation

--- kmain.c
+++ kmain.c
@@ -6,27 +6,23 @@
 
 #define NB_PROCESS 5
 
-void user_process()
-{
-int v=0;
-for(;;)
-{
-v++;
-sys_yield();
+void user_process() {
+	int v=0;
+	for(;;){
+		v++;
+		sys_yield();
+	}
 }
-}
-void kmain( void )
-{
-sched_init();
-int i;
-for(i=0;i<NB_PROCESS;i++)
-{
-create_process((func_t *) &user_process);
-}
-__asm("cps 0x10"); // switch CPU to USER mode
-// **********************************************************************
-while(1)
-{
-sys_yield();
-}
+
+void kmain( void ) {
+	sched_init();
+	int i;
+	for(i=0;i<NB_PROCESS;i++){
+		create_process((func_t *) &user_process);
+	}
+	__asm("cps 0x10"); // switch CPU to USER mode
+	// **********************************************************************
+	while(1) {
+		sys_yield();
+	}
 }
