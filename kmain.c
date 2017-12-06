b65200924b842539ee34cfdb1ef058d2c2d56e3a
GuillaumeFerron <gferron@TC405-112-05.insa-lyon.fr>
2017-12-06 08:20:14 +0100

    chapter 6 kmain modifications

--- kmain.c
+++ kmain.c
@@ -4,39 +4,29 @@
 #include "./src/util.h"
 #include "sched.h"
 
-//Function declarations
-void user_process_1();
-void user_process_2();
+#define NB_PROCESS 5
 
-//PCBs
-struct pcb_s *p1, *p2;
-
-void kmain( void ) {
-	sched_init();
-
-	p1 = create_process((func_t*) &user_process_1);
-	p2 = create_process((func_t*) &user_process_2);
-
-	__asm("cps 0x10"); // switch CPU to USER mode
-	// **********************************************************************
-	sys_yieldto(p1);
-
-	// this is now unreachable
-	PANIC();
+void user_process()
+{
+int v=0;
+for(;;)
+{
+v++;
+sys_yield();
 }
-
-void user_process_1() {
-	int v1=5;
-	while(1) {
-		v1++;
-		sys_yieldto(p2);
-	}
 }
-
-void user_process_2() {
-	int v2=-12;
-	while(1) {
-		v2-=2;
-		sys_yieldto(p1);
-	}
+void kmain( void )
+{
+sched_init();
+int i;
+for(i=0;i<NB_PROCESS;i++)
+{
+create_process((func_t *) &user_process);
+}
+__asm("cps 0x10"); // switch CPU to USER mode
+// **********************************************************************
+while(1)
+{
+sys_yield();
+}
 }
